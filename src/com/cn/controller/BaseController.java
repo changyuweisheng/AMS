5a956bea5717c1c95d8ffb7af0ea3b8e795e5c52
changyuweisheng <3102973043@qq.com>
2019-06-30 10:38:58 +0800

    123

---
+++ src/com/cn/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.cn.controller;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+public class BaseController {
+	/**
+	 * 使用@InitBinder解决SpringMVC日期类型无法绑定的问题
+	 * @param dataBinder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder){
+		System.out.println("initBinder=======================");
+		  
+		dataBinder.registerCustomEditor(Date.class, 
+				new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
+	 	
+	}
+}
